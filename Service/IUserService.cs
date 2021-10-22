b6784f070cf4cdcd0144d150f178a2a1fd18cd88
Sayeem <16103179@iubat.edu>
2021-10-22 17:14:57 +0600

    Add project files.

---
+++ Service/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using JWTAuthApi.Entities;
+using JWTAuthApi.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthApi.Service
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,63 @@
+﻿using JWTAuthApi.Entities;
+using JWTAuthApi.Helper;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthApi.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly AppSettings _appSettings;
+        private readonly JWTAuthApiDBContext _context;
+
+        public UserService(IOptions<AppSettings> appSettings, JWTAuthApiDBContext context)
+        {
+            _appSettings = appSettings.Value;
+            _context = context;
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _context.Users.SingleOrDefault(x => x.UserName == model.Username && x.Password == model.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponse(user, "Bearer " + token);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users;
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(x => x.Id == id);
+        }
+        private string generateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
